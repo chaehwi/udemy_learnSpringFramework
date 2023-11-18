2de56dd5d35b85e60b760a2bf0662921b395366e
chaehwi <98143959+chaehwi@users.noreply.github.com>
2023-11-18 23:15:38 +0900

    AnnotationConfigApplicationContext

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -15,7 +15,18 @@ public class App02HelloWorldSpring {
 
 		// 3. 스프링이 관리하는 Bean 을 검색
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
 		
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("person2MethodCall"));
+		System.out.println(context.getBean("person3Parameters"));
+
+		System.out.println(context.getBean("address2"));
+		System.out.println(context.getBean("address3"));
+
+//		System.out.println(context.getBean(Address.class)); // Bean 의 유형은 Bean 의 Bean, 일치하는 것이 두 개(address2,address3)
+		// 스프링이 Bean 을 관리하기 시작하면 Bean 의 이름을 사용하거나 유형을 사용해 검색 및 이용 가능
+
 	}
 
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,53 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// record 는 jdk16에서 추가된 새로운 기능
+// Java Bean 을 만드는 번거로움을 없애기 위해 도입된 기능
+// record 를 사용하면 세터 게터 생성자 등을 만들 필요가 없음. 자동 생성.
+record Person(String name, int age, Address address) {
+};
+
+record Address(String firstLine, String city) {
+};
+
 @Configuration
 public class HelloWorldConfiguration {
 
 	@Bean
 	public String name() {
 		return "Ranga";
+	}
+
+	@Bean
+	public int age() {
+		return 15;
+	}
+
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20, address());
+	}
+
+	// 기존 스프링 bean 과 관계가 있는 새로운 bean 만들기
+	// 1. 메서드 호출 방법
+	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address());
+	}
+
+	// 2. 매개 변수 방법
+	@Bean
+	public Person person3Parameters(String name, int age, Address address2) {
+		return new Person(name, age, address2);
+	}
+
+	@Bean(name = "address2") // Bean 의 이름을 사용자 지정할 수 있음
+	public Address address() {
+		return new Address("Suyeong", "Busan");
+	}
+
+	@Bean(name = "address3") // Bean 의 이름을 사용자 지정할 수 있음
+	public Address address3() {
+		return new Address("Suyeong3", "Busan3");
 	}
 }
